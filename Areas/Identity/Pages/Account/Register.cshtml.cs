65a87bc266bb6de11e79448317719de36015fb14
Dima Tarasov <tarasov14.08.2002@gmail.com>
2026-09-25 14:04:41 +0300

    change validation of password in registration

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -62,7 +62,6 @@ namespace resumeSystem.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
